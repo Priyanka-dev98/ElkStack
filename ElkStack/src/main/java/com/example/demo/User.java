2e9f4654d120ee486b6286668f5107a3ed511dc4
infyshine <infyshine@DESKTOP-HITNQSA>
2021-08-13 15:09:27 +0530

    es

---
+++ ElkStack/src/main/java/com/example/demo/User.java
@@ -0,0 +1,34 @@
+package com.example.demo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Data
+public class User {
+ 
+	private int  id;
+	private String name;
+	public User(int i, String string) {
+		// TODO Auto-generated constructor stub
+	}
+	public User() {
+		// TODO Auto-generated constructor stub
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+}
